6b3273d5287db3de8a7c8f5008b99417d49f3e94
Harsaroop Singh <harsaroop.singh0530@gmail.com>
2020-07-11 00:15:51 +0530

    Update solution_python.py
    Labels:
    COOP Fall 2020

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,37 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.list = []
+        self.un = 0
+        self.list_un = []
 
+        
     def add(self, num: int):
-        pass
+        self.list.append(num)
+        self.value += num 
 
     def subtract(self, num: int):
-        pass
+        e = -1 * num
+        self.list.append(e)
+        self.value = self.value - num       
 
     def undo(self):
-        pass
+        self.un = self.list.pop()
+        self.value = self.value - self.un
 
     def redo(self):
-        pass
+        self.list.append(self.un)
+        self.value = self.value + self.un
 
     def bulk_undo(self, steps: int):
-        pass
+        for i in range(steps):
+            self.un = self.list.pop()
+            self.value = self.value - self.un
+            self.list_un.append()
 
     def bulk_redo(self, steps: int):
-        pass
+        for i in range(steps):
+            self.un = self.list_un.pop()
+            self.value = self.value + self.un
+            self.list.append(self.un)
+
